0cfcd1350da13b0786d1189bb3e7c5d037d5fb5d
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-05-16 12:06:35 +0545

    Added children props

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,5 @@
 import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
@@ -8,7 +9,7 @@ function ExpenseDate(props) {
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
-      <div className="expense-day__day">{day}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
