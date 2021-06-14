49f19e1daf3dc03be178ae0bc4fcaa705b93bcfe
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-06-14 19:43:14 +0545

    Added chartbar and finished expenses tracker

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,10 +4,18 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className='chart'>
       {props.dataPoints.map((dataPoint) => (
-        <ChartBar value={dataPoint.value}  maxValue = {null} label{dataPoint.label} key={dataPoint.label} />
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
       ))}
     </div>
   );
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import './ChartBar.css';
 
 const ChartBar = (props) => {
   let barFillHeight = '0%';
 
-  if (props.max > 0) {
-    barFillHeight = Math.round((props.value / props.maxvalue) * 100 + '%');
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
 
   return (
@@ -15,8 +16,8 @@ const ChartBar = (props) => {
           className='chart-bar__fill'
           style={{ height: barFillHeight }}
         ></div>
-        <div className='chart-bar__label'>{props.label}</div>
       </div>
+      <div className='chart-bar__label'>{props.label}</div>
     </div>
   );
 };
